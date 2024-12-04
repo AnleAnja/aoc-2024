541a2338c66fea42d4fcafc6c6d133fd0b48fa89
AnleAnja <anleanja@live.de>
2024-12-04 21:30:37 +0100

    solve day 2 part 1

--- src/day2/main.kt
+++ src/day2/main.kt
@@ -1,2 +1,15 @@
 package day2
 
+import java.io.File
+
+var list = listOf<List<Int>>()
+
+fun main() {
+    getInput("src/day2/input.txt")
+    println(day2part1(list))
+}
+
+fun getInput(input: String) {
+    list = File(input).readLines()
+        .map { line -> line.split(" ").map { it.toInt() } }
+}
--- src/day2/part1.kt
+++ src/day2/part1.kt
@@ -1,2 +1,18 @@
 package day2
 
+import kotlin.math.abs
+
+fun day2part1(reports: List<List<Int>>): Int {
+    val count = mutableListOf<Boolean>()
+    reports.forEach { report ->
+        val repAsc = report == report.sorted()
+        val repDesc = report == report.sortedDescending()
+        val hasDiff = report.zipWithNext().all { (a, b) ->
+                val diff = abs(a - b)
+                diff in 1..3
+            }
+            val safe = (repAsc || repDesc) && hasDiff
+            count.add(safe)
+    }
+    return count.count { it }
+}
