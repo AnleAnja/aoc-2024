0138ae246d8a8949daf3a88c637d1be11f04d87d
AnleAnja <anleanja@live.de>
2024-12-05 20:52:06 +0100

    solve part 2

--- src/day2/part2.kt
+++ src/day2/part2.kt
@@ -1,2 +1,16 @@
 package day2
 
+fun day2part2(reports: List<List<Int>>): Int {
+    val count = mutableListOf<Boolean>()
+    reports.forEach { report ->
+        var safe = checkSafeness(report)
+        if (!safe) {
+            safe = report.indices.any { i ->
+                checkSafeness(report.filterIndexed { index, _ -> index != i })
+            }
+        }
+        count.add(safe)
+    }
+    return count.count { it }
+}
+
