344fc591dcf24c5459cfe877b798c10392031ca5
AnleAnja <anleanja@live.de>
2024-12-05 20:51:52 +0100

    outsource logic for day 2

--- src/day2/main.kt
+++ src/day2/main.kt
@@ -1,15 +1,27 @@
 package day2
 
 import java.io.File
+import kotlin.math.abs
 
 var list = listOf<List<Int>>()
 
 fun main() {
     getInput("src/day2/input.txt")
     println(day2part1(list))
+    println(day2part2(list))
 }
 
 fun getInput(input: String) {
     list = File(input).readLines()
         .map { line -> line.split(" ").map { it.toInt() } }
+}
+
+fun checkSafeness(report: List<Int>): Boolean {
+    val repAsc = report == report.sorted()
+    val repDesc = report == report.sortedDescending()
+    val hasDiff = report.zipWithNext().all { (a, b) ->
+        val diff = abs(a - b)
+        diff in 1..3
+    }
+    return (repAsc || repDesc) && hasDiff
 }
--- src/day2/part1.kt
+++ src/day2/part1.kt
@@ -1,18 +1,9 @@
 package day2
 
-import kotlin.math.abs
-
 fun day2part1(reports: List<List<Int>>): Int {
     val count = mutableListOf<Boolean>()
     reports.forEach { report ->
-        val repAsc = report == report.sorted()
-        val repDesc = report == report.sortedDescending()
-        val hasDiff = report.zipWithNext().all { (a, b) ->
-                val diff = abs(a - b)
-                diff in 1..3
-            }
-            val safe = (repAsc || repDesc) && hasDiff
-            count.add(safe)
+        count.add(checkSafeness(report))
     }
     return count.count { it }
 }
